cc62b9c5360a2c8ced2f6d70c8b5e27ce77adde2
Jaime de Greiff <alegreiff@gmail.com>
2022-02-22 21:38:43 -0500

    jaimedegreiff.ml

---
+++ pages/posts/[id].jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../../componentes/UI/Layout";
+
+export default function SinglePost() {
+  return <Layout>El IDD</Layout>;
+}
--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,26 +1,50 @@
-import { Grid } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
 import fetch from "isomorphic-fetch";
+import Link from "next/link";
 import React from "react";
 import Layout from "../../componentes/UI/Layout";
 
 function PostIndexPage({ posts }) {
   return (
     <Layout>
-      <Grid container>
-        <Grid item xs={12}>
-          {posts.map((post) => (
-            <div key={post._id}>
-              <h5>{post.tituloEntrada}</h5>
-            </div>
-          ))}
-        </Grid>
+      <Grid container spacing={3}>
+        {posts.map((post) => (
+          <Grid item xs={6} key={post._id}>
+            <Card>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 22 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {post.tituloEntrada}
+                </Typography>
+
+                <Typography variant="body2">{post.contenido}</Typography>
+              </CardContent>
+              <CardActions>
+                <Link href={`/posts/${post._id}`}>
+                  <Button size="small">Learn More</Button>
+                </Link>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Layout>
   );
 }
 
 PostIndexPage.getInitialProps = async ({ origin }) => {
-  const res = await fetch("https://mongoreact-alegreiff.vercel.app/api/posts");
+  //const res = await fetch("https://jaimedegreiff.ml/api/posts");
+  const res = await fetch("http://localhost:3000/api/posts");
   const { data } = await res.json();
   return { posts: data };
 };
