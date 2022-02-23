ef14d4fe514b8d844950d74993336c2e0fd66423
Jaime de Greiff <alegreiff@gmail.com>
2022-02-23 12:17:29 -0500

    local post solved

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -3,7 +3,5 @@ import React from "react";
 import Layout from "../../componentes/UI/Layout";
 
 export default function SinglePost() {
-  const router = Router();
-  console.log(router)
   return <Layout>El IDD</Layout>;
 }
