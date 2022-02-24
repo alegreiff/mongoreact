f299299edfe74a37c3f7414e37005e53582cf5d3
Jaime de Greiff <alegreiff@gmail.com>
2022-02-24 14:57:50 -0500

    nprogres

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,51 @@ import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
 import "../css/estilos.css";
 
+import nprogress from "nprogress";
+import { useRouter } from "next/router";
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      if (shallow) {
+        nprogress.start();
+      } else {
+        nprogress.done();
+      }
+      console.log("oh shallouuuu", shallow);
+      console.log(
+        `App is changing to ${url} ${
+          shallow ? "with" : "without"
+        } shallow routing`
+      );
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
